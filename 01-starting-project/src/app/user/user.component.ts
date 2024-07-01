f376bff6a5145d92ebac406c826f610b96a05a3f
Muhammad Hussain Javed <k213584@nu.edu.pk>
2024-07-01 23:53:45 +0500

    Completed dynamic getters

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+// Create data for random user we will import later dynamically
+export class UserComponent {
+  // This property is now available in the user.component.html template
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  // Create a getter which generates the path to the images
+  get imagePath(){
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
