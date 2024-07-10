5aadf3a8a9ccf9f98f53825729b5dc45a038aada
hussain <hussainjavedcys@gmail.com>
2024-07-11 00:58:30 +0500

    Completed custom two-way binding

--- 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,8 +9,16 @@ import { Component } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
+  // size = input.required<{width: string; height: string;}>();
+  // sizeChange = output<{width: string; height: string;}>()
+
+  // A newer way to make custom two-way binding
+  size = model.required<{width: string; height: string;}>();
 
   onReset() {
-    // ...
+    this.size.set({
+      width: '200',
+      height: '100'
+    })
   }
 }
