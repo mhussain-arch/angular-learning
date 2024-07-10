60844124ac9c04697a73450cb99c46665bf2d4bd
hussain <hussainjavedcys@gmail.com>
2024-07-11 00:44:47 +0500

    Completed ticket submission & signals part

--- 06-component-deepdive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-component-deepdive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, effect, OnDestroy, OnInit, signal } from '@angular/core';
 import { DashboardItemComponent } from "../dashboard-item/dashboard-item.component";
 
 @Component({
@@ -9,22 +9,27 @@ import { DashboardItemComponent } from "../dashboard-item/dashboard-item.compone
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent implements OnInit, OnDestroy {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus = signal<'online'| 'offline'| 'unknown'>('offline');
   private interval?: ReturnType<typeof setInterval>;
-  constructor(){}
+  constructor(){
+    effect(() => {
+      console.log(this.currentStatus()); // Will output whenever value changes
+    })
+    // console.log(this.currentStatus()); // Only executes once
+  }
 
   ngOnInit(){
     this.interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5){
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       }
       else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       }
       else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
   }
