05524a861a5e6c281b9e9b8eccf6e756c1608cc0
Hussain <hussainjavedcys@gmail.com>
2024-07-17 21:55:38 +0500

    Completed till form validation

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
+  imports: [FormsModule],
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmitForm(formData: NgForm){
+    if (formData.form.invalid){
+      return;
+    }
+    const enteredEmail = formData.form.value.email;
+    const eneteredPassword = formData.form.value.password;
+    console.log(enteredEmail, eneteredPassword);
+    console.log(formData.form.status);
+  }
+}
